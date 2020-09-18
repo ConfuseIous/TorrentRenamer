252e392c6c8e8caccedfc9b55d582beffbb80f36
Karandeep Singh <karandeepsingh@Karans-MacBook-Pro.local>
2020-09-18 18:48:17 +0800

    Fixed

--- MusicFolderRenamer.py
+++ MusicFolderRenamer.py
@@ -1,28 +1,47 @@
 import os
 
-path = ""
+##REMOVE BLINK-182 FROM FOLDER BEFORE RUNNING THIS
 
-while path == "":
-        path = input("Enter folder location in system directory (eg- /users/username/desktop/foldername): ")
+artistPath = "/users/karandeepsingh/desktop/music"
 
-        try:
-            os.listdir(path)
-
-        except:
-            print("Invalid filepath, try again")
-            path = ""
-
-path.strip()
+artistPath.strip()
 
 try:
-        location = path + "/" + ".DS_Store"
+        location = artistPath + "/" + ".DS_Store"
         os.remove(location)
 
 except:
         print("No rubbish files found for deletion")
 
-for f in os.listdir(path):
-    print(f)
+sorted_artists = sorted(os.listdir(artistPath))
 
-sorted_files = sorted(os.listdir(path))
+print(sorted_artists)
+
+for artist in sorted_artists:
+        
+        albumPath = artistPath + "/" + artist
+        os.chdir(albumPath)
+
+        try:
+                location = albumPath + "/" + ".DS_Store"
+                os.remove(location)
+
+        except:
+               print("No rubbish files found for deletion")
+
+        sorted_albums = sorted(os.listdir(albumPath))
+
+        for album in sorted_albums:
+                
+               rem_i = album.find("-")
+
+               if rem_i != -1:
+                       end = len(album)
+                       new_str = album[rem_i+1:end]
+                       new_str.strip
+                       os.rename(album, new_str)
+
+
+        
+
 
