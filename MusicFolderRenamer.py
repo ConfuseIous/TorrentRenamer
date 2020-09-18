9c1893e828b3298cbf551e53475e92c53ef37e4f
Karandeep Singh <karandeepsingh@Karans-MacBook-Pro.local>
2020-09-18 17:58:28 +0800

    new file

--- MusicFolderRenamer.py
+++ MusicFolderRenamer.py
@@ -2,7 +2,7 @@ import os
 
 path = ""
 
-while path == "":   
+while path == "":
         path = input("Enter folder location in system directory (eg- /users/username/desktop/foldername): ")
 
         try:
@@ -26,15 +26,3 @@ for f in os.listdir(path):
 
 sorted_files = sorted(os.listdir(path))
 
-newname = input("What should the files be named? (eg- Breaking Bad S01E will return Breaking Bad S01E01...): ")
-ext = input("What is the file format? (mkv/mp4/jpg/png/etc): ")
-
-os.chdir(path)
-
-for index, filename in enumerate(sorted_files, 1):
-        
-        name = f"{newname}{index:02d}.{ext}"
-
-        print("renaming " + filename + " to " + name) 
-
-        os.rename(filename, name)
